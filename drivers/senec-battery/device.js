0edd17ecfed2c9157767c0b36115a9903c91debe
Daniel <info@daniel-b.de>
2025-01-11 23:02:06 +0100

    add support for "force charging"

--- drivers/senec-battery/device.js
+++ drivers/senec-battery/device.js
@@ -9,6 +9,14 @@ class SenecDevice extends Homey.Device {
   async onInit() {
     this.log("SenecDevice has been initialized");
 
+    this.registerCapabilityListener("onoff", async (value) => {
+      if (value) {
+        this.forceCharge();
+      } else {
+        this.allowDischarge();
+      }
+    });
+
     this.pollInterval = setInterval(() => {
       this.pollBatteryStatus();
     }, 3000);
@@ -19,27 +27,25 @@ class SenecDevice extends Homey.Device {
     try {
       process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-      const ipAddress = this.getSetting('ipAddress');
+      const ipAddress = this.getSetting("ipAddress");
 
       var http = require("http.min");
-      var result = await http.post(
-        "https://" + ipAddress + "/lala.cgi",
-        {
-          BMS: {
-            SYSTEM_SOC: "",
-          },
-          ENERGY: {
-            GUI_INVERTER_POWER: "",
-            GUI_BAT_DATA_VOLTAGE: "",
-            GUI_BAT_DATA_CURRENT: "",
-            GUI_BAT_DATA_POWER: "",
-            GUI_INVERTER_POWER: "",
-            GUI_HOUSE_POW: "",
-            GUI_GRID_POW: "",
-            GUI_CHARGING_INFO: "",
-          },
-        }
-      );
+      var result = await http.post("https://" + ipAddress + "/lala.cgi", {
+        BMS: {
+          SYSTEM_SOC: "",
+        },
+        ENERGY: {
+          GUI_INVERTER_POWER: "",
+          GUI_BAT_DATA_VOLTAGE: "",
+          GUI_BAT_DATA_CURRENT: "",
+          GUI_BAT_DATA_POWER: "",
+          GUI_INVERTER_POWER: "",
+          GUI_HOUSE_POW: "",
+          GUI_GRID_POW: "",
+          GUI_CHARGING_INFO: "",
+          SAFE_CHARGE_RUNNING: "",
+        },
+      });
 
       //this.log("Response: " + result.data);
 
@@ -53,6 +59,20 @@ class SenecDevice extends Homey.Device {
       let inverterPower = jsonData.ENERGY.GUI_INVERTER_POWER.substring(3);
       let gridPower = jsonData.ENERGY.GUI_GRID_POW.substring(3);
       let housePower = jsonData.ENERGY.GUI_HOUSE_POW.substring(3);
+
+      let isSafeChargeRunning = jsonData.ENERGY.SAFE_CHARGE_RUNNING;
+
+      console.log();
+
+      if (isSafeChargeRunning == "u8_00") {
+        if (this.getCapabilityValue("onoff")) {
+          this.setCapabilityValue("onoff", false).catch(this.error);
+        }
+      } else {
+        if (!this.getCapabilityValue("onoff")) {
+          this.setCapabilityValue("onoff", true).catch(this.error);
+        }
+      }
 
       this.setCapabilityValue(
         "measure_voltage",
@@ -115,6 +135,48 @@ class SenecDevice extends Homey.Device {
       exp--;
     }
     return float * sign;
+  }
+
+  async forceCharge() {
+    try {
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+      const ipAddress = this.getSetting("ipAddress");
+
+      var http = require("http.min");
+      var result = await http.post("https://" + ipAddress + "/lala.cgi", {
+        ENERGY: {
+          SAFE_CHARGE_FORCE: "u8_01",
+        },
+      });
+
+      this.log("Response: " + result.data);
+
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+    } catch (error) {
+      this.error("Fehler bei der Abfrage des Batterie-Status", error);
+    }
+  }
+
+  async allowDischarge() {
+    try {
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+      const ipAddress = this.getSetting("ipAddress");
+
+      var http = require("http.min");
+      var result = await http.post("https://" + ipAddress + "/lala.cgi", {
+        ENERGY: {
+          SAFE_CHARGE_PROHIBIT: "u8_01",
+        },
+      });
+
+      this.log("Response: " + result.data);
+
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+    } catch (error) {
+      this.error("Fehler bei der Abfrage des Batterie-Status", error);
+    }
   }
 
   /**
